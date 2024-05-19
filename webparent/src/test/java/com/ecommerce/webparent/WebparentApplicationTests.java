23e54c7e5c22f8de1abe4e847508dee4ad519ff8
Lorenzo Cardinali <buffon111@hotmail.it>
2024-05-19 23:19:44 +0200

    Added draft

---
+++ webparent/src/test/java/com/ecommerce/webparent/WebparentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.webparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebparentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
